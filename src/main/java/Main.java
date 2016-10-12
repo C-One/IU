a898904f60b3e67226f3b75b8e3396426d0ce981
jykang <junyoung.kang@navercorp.com>
2016-10-12 21:56:38 +0900

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by jykang on 2016. 10. 12..
+ */
+public class Main {
+
+}
---
+++ src/main/java/gray/c/one/Gray.java
@@ -0,0 +1,7 @@
+package gray.c.one;
+
+/**
+ * Created by jykang on 2016. 10. 12..
+ */
+public class Gray {
+}
