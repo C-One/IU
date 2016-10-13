f21c0221ddc3e27546c915d9892c0d8ffe475d61
jykang <junyoung.kang@navercorp.com>
2016-10-13 11:29:32 +0900

    spring boot initial commit

---
+++ src/main/java/gray/c/one/IuApplication.java
@@ -0,0 +1,12 @@
+package gray.c.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuApplication.class, args);
+	}
+}
